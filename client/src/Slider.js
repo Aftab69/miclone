e4500087a8d7df777c058d1674412af57a006778
Aftab69 <aftabhssn3@gmail.com>
2022-03-10 17:51:56 +0530

    carousel fixed

--- client/src/Slider.js
+++ client/src/Slider.js
@@ -1,46 +1,28 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import Data from './data/data.json';
 
 const Slider = () => {
+  const dataArray = Data.banner.start;
+  
   return (
     <>
-          <Carousel fade>
+    <Carousel fade>
+    {
+      dataArray.map((eachDataArrayElement)=>(   
             <Carousel.Item interval={2000}>
               <img 
                 className='d-block w-100'
-                src="https://images.pexels.com/photos/341523/pexels-photo-341523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                alt="First slide"
+                src={eachDataArrayElement}
+                alt={eachDataArrayElement}
               />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={2000}>
-              <img
-                className='d-block w-100'
-                src="https://images.pexels.com/photos/40739/mobile-phone-smartphone-tablet-white-40739.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Second slide"
-              />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={2000}>
-              <img
-                className='d-block w-100'
-                src="https://images.pexels.com/photos/3973973/pexels-photo-3973973.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="Third slide"
-              />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-      </Carousel>
+      ))
+    }   
+    </Carousel>     
     </>
   )
+
 }
 
 export default Slider;
